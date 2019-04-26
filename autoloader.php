74b8f64b1ac3981e7883f6f50259fe4a2f66a92f
Paul Huig <phuig0@xs4all.net>
2019-04-26 21:23:03 +0200

    added DIRECTORY_SEPERATOR in case this code runs on a different machine

--- autoloader.php
+++ autoloader.php
@@ -6,7 +6,7 @@ spl_autoload_register(function ($class) {
     $prefix = 'Tweakers\\';
 
     // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/src/';
+    $base_dir = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
@@ -21,7 +21,7 @@ spl_autoload_register(function ($class) {
     // replace the namespace prefix with the base directory, replace namespace
     // separators with directory separators in the relative class name, append
     // with .php
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    $file = $base_dir . str_replace('\\', DIRECTORY_SEPARATOR, $relative_class) . '.php';
 
     // if the file exists, require it
     if (file_exists($file)) {
